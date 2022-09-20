9d53dd398d08b806263275ed0080ce30ae6cf386
Maripandi P <maripandi422@outlook.com>
2022-09-20 09:38:06 +0530

    home page UI changed, added get a Quote in home page and contact us page

--- app/models.py
+++ app/models.py
@@ -1,10 +1,7 @@
 
 
 
-import email
-from operator import mod
-from turtle import position
-from urllib import response
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.db.models.signals import post_save
